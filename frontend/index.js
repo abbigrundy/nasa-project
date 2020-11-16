00046180fde3ff548acaa99cf8d5fa77a6dd8d65
abbigrundy <abbi.grundy@bbc.co.uk>
2020-11-16 16:10:01 +0000

    display the current sol of the curiosity rover

--- frontend/index.js
+++ frontend/index.js
@@ -27,3 +27,31 @@ const timeZoneDifference = () => {
 };
 
 timeZoneDifference();
+
+// A sol is slightly longer than an Earth day. It is approximately 24 hours, 39 minutes, 35 seconds long.
+// earth day in milliseconds
+// sol day in milliseconds
+// date when sol landed on mars - 08/08/2012 time 05:17:57 - dont forget it is 0 indexed.
+// divide sol day by earth day
+
+const solDays = document.getElementById("sol-days");
+const earthDayMilliseconds = 86400000;
+const solDayMilliseconds = 88775240;
+const convertToSol = solDayMilliseconds / earthDayMilliseconds;
+
+// display the current sol of the curiosity rover
+const calcualteSol = () => {
+  // landing date and use method to get the miliseconds
+  const curiosityLandingDateMilliseconds =
+    Date.now() - new Date(2012, 7, 6, 5, 17, 57).getTime();
+  // earth day since landing is sol millisecond / earth Day milliseconds = 1.02
+
+  const earthDaySinceLanding =
+    curiosityLandingDateMilliseconds / earthDayMilliseconds;
+  // number of sols = divided by convert to sol (solday/earthday)
+
+  const calculate = earthDaySinceLanding / convertToSol;
+  const result = Math.round(calculate);
+  solDays.innerHTML = result
+};
+calcualteSol();
