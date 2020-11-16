8079f9ed4022664c71ce1f099fbf630b2ad82d31
abbigrundy <abbi.grundy@bbc.co.uk>
2020-11-16 13:42:18 +0000

    new endpoint for login and form created

--- routes.js
+++ routes.js
@@ -3,13 +3,12 @@ const express = require("express");
 const router = express.Router();
 
 router.use(express.static("frontend"));
-// router.use(express.static("frontend/login.html"));
+
 router.use("/login", express.static("frontend/login.html"));
 
-router.get("/", (req, res) => {
-  res.send();
-});
-
+router.post("/authenticate", (req,res) => {
+  
+})
 router.get("/image", (req, res) => {
   res.send("This is the photo of the day");
 });
