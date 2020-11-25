48b910596e426719376bf7f982be97ec98510c8d
abbigrundy <abbi.grundy@bbc.co.uk>
2020-11-25 11:20:36 +0000

    display a image of the mars rover path with responsive design

--- routes.js
+++ routes.js
@@ -6,13 +6,13 @@ router.use(express.static("frontend"));
 
 router.use("/login", express.static("frontend/login.html"));
 
-router.post("/authenticate", (req, res) => {});
+router.post("/authenticate", (req, res) => {
+  // TODO:
+});
 router.get("/image", (req, res) => {
   res.send("This is the photo of the day");
 });
 
-router.get("/rover", (req, res) => {
-  res.send("Rover Page");
-});
+router.use("/rover", express.static("frontend/mars-rover.html"));
 
 module.exports = router;
