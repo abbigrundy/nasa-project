ba311726ac93dbf45a9707184d321d4a45856ed7
abbigrundy <abbi.grundy@bbc.co.uk>
2020-11-25 10:23:06 +0000

    new endpoint for rover

--- routes.js
+++ routes.js
@@ -6,11 +6,13 @@ router.use(express.static("frontend"));
 
 router.use("/login", express.static("frontend/login.html"));
 
-router.post("/authenticate", (req,res) => {
-  
-})
+router.post("/authenticate", (req, res) => {});
 router.get("/image", (req, res) => {
   res.send("This is the photo of the day");
+});
+
+router.get("/rover", (req, res) => {
+  res.send("Rover Page");
 });
 
 module.exports = router;
