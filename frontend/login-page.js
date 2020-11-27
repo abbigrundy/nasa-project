7fe825a08829f508bf5245214f86aea1393f9840
abbigrundy <abbi.grundy@bbc.co.uk>
2020-11-27 10:53:05 +0000

    refactor login page to start implementing jwt fresh

--- frontend/login-page.js
+++ frontend/login-page.js
@@ -1,6 +1,3 @@
-const credentials = require("../../../Bootcamp1/bookish/credentials");
-import { credentials } from "./credentials";
-
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
@@ -10,7 +7,7 @@ loginButton.addEventListener("click", (e) => {
   const username = loginForm.username.value;
   const password = loginForm.password.value;
 
-  if (username === credentials.username && password === credentials.password) {
+  if (username === "abbi" && password === "grundy") {
     alert("You have successfully logged in.");
     location.reload();
   } else {
