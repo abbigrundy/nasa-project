34c1072512f31adaed25fe7c3cbf8cd0f9405186
abbigrundy <abbi.grundy@bbc.co.uk>
2020-12-14 09:21:13 +0000

    fixed files and re commited database work

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const router = require("./routes");
+const router = require("./server/routes");
+
+const passport = require("passport");
+const cookieParser = require("cookie-parser");
+const { passportConfiguration } = require("./server/passportConfiguration");
+
+passportConfiguration();
+
+app.use(passport.initialize());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(router);
-
 app.listen(port);
